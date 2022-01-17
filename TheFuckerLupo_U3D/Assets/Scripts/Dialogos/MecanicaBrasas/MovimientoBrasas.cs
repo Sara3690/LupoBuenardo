10cc64829501e2c1f5b9b3466a2bf7fd82f8d8ee
PC14\Alumno <cerezogp@gmail.com>
2022-01-17 14:30:36 +0100

    Arreglar brasas

--- TheFuckerLupo_U3D/Assets/Scripts/Dialogos/MecanicaBrasas/MovimientoBrasas.cs
+++ TheFuckerLupo_U3D/Assets/Scripts/Dialogos/MecanicaBrasas/MovimientoBrasas.cs
@@ -9,6 +9,8 @@ public class MovimientoBrasas : MonoBehaviour
 
     UnityEngine.AI.NavMeshAgent nav;
 
+    public Animator movBrasas;
+
 
 
     private void Start()
@@ -19,12 +21,32 @@ public class MovimientoBrasas : MonoBehaviour
 
     private void Update()
     {
-        nav.SetDestination(jugador.transform.position);
+       
+        if (Vector3.Distance(transform.position,jugador.transform.position) <=10)
+        {
 
-        Vector3 direction = jugador.transform.position;
+            nav.SetDestination(jugador.transform.position);
 
-        transform.LookAt(direction);
+            Vector3 direction = jugador.transform.position;
 
+            transform.LookAt(direction);
+
+            movBrasas.SetBool("Correr", true);
+            movBrasas.SetBool("Parar", false);
+
+        }
+        else
+        {
+            movBrasas.SetBool("Parar", true);
+            movBrasas.SetBool("Correr", false);
+
+        }
+        if (Vector3.Distance(transform.position, jugador.transform.position) <= 4)
+        {
+            movBrasas.SetBool("Correr", false);
+            movBrasas.SetBool("Parar", true);
+        }
+        
     }
 
 }
