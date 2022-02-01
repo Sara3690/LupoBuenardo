37459ba77eeca8329ec4b44e1ed40fbbc24f3cf0
PC14\Alumno <cerezogp@gmail.com>
2022-02-01 13:37:34 +0100

    arreglaciones

--- TheFuckerLupo_U3D/Assets/Scripts/Pruebas/Camara/SigueAlJugador.cs
+++ TheFuckerLupo_U3D/Assets/Scripts/Pruebas/Camara/SigueAlJugador.cs
@@ -16,6 +16,7 @@ public class SigueAlJugador : MonoBehaviour
     public CambioPanaderias panaderias;
     public CambioMolinos molinos;
 
+    public int zoom;
 
     private void Start()
     {
@@ -27,50 +28,63 @@ public class SigueAlJugador : MonoBehaviour
     {
         transform.position = new Vector3( alturaX + jugador.transform.position.x, alturaY + jugador.transform.position.y, alturaZ + jugador.transform.position.z );
 
-        if (casaLupo.cambioZoom == 0)
+        if (zoom == 0)
         {
             Camera.main.fieldOfView = 34f;
 
             alturaY = 10.47f;
             alturaZ = -14.9f;
-            
+
+            this.transform.rotation = Quaternion.Euler(30f, 0, 0);
+
         }
 
-        if (casaLupo.cambioZoom == 1)
+        if (zoom == 1)
         {
-            Camera.main.fieldOfView = 50f;
+            Camera.main.fieldOfView = 42.2f;
 
-            alturaY = 12f;
-            alturaZ = -12f;
+            alturaY = 8f;
+            alturaZ = -17f;
+
+            this.transform.rotation = Quaternion.Euler(17.7f, 0, 0);
 
         }
 
-        if (panaderias.cambioZoom == 2)
-        {
-            Camera.main.fieldOfView = 34f;
-            alturaY = 10.47f;
-            alturaZ = -14.9f;
-        }
-
-        if (panaderias.cambioZoom == 3)
-        {
-            Camera.main.fieldOfView = 50f;
-            alturaY = 12f;
-            alturaZ = -12f;
-        }
-
-        if (molinos.cambioZoom == 4)
+        if (zoom == 2)
         {
             Camera.main.fieldOfView = 34f;
             alturaY = 10.47f;
             alturaZ = -14.9f;
+
+            this.transform.rotation = Quaternion.Euler(30f, 0, 0);
         }
 
-        if (molinos.cambioZoom == 5)
+        if (zoom == 3)
         {
-            Camera.main.fieldOfView = 50f;
-            alturaY = 12f;
-            alturaZ = -12f;
+            Camera.main.fieldOfView = 42.2f;
+
+            alturaY = 8f;
+            alturaZ = -17f;
+
+            this.transform.rotation = Quaternion.Euler(17.7f, 0, 0);
+        }
+
+        if (zoom == 4)
+        {
+            Camera.main.fieldOfView = 34f;
+            alturaY = 10.47f;
+            alturaZ = -14.9f;
+            this.transform.rotation = Quaternion.Euler(30f, 0, 0);
+        }
+
+        if (zoom == 5)
+        {
+            Camera.main.fieldOfView = 42.2f;
+
+            alturaY = 8f;
+            alturaZ = -17f;
+
+            this.transform.rotation = Quaternion.Euler(17.7f, 0, 0);
         }
 
     }
